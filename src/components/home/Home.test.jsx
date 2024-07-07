6ff71443963aa251a30c19a967f0b926daa3b4ae
DenPro117 <wangpanpan227@gmail.com>
2024-07-07 20:55:59 +0800

    Fix: static text

--- src/components/home/Home.test.jsx
+++ src/components/home/Home.test.jsx
@@ -4,7 +4,7 @@ import { WELCOME_HEADING } from '../../constants/HomeConstants';
 import Home from './Home';
 
 describe('[Home Component Test Suite]', () => {
-  test('should pass if "React Enterprise Starter Kit" is found in the document', () => {
+  test('should pass if "Lomo App Built With React" is found in the document', () => {
     render(<Home />);
     expect(screen.getByText(WELCOME_HEADING)).toBeInTheDocument();
   });
