0d7ddd41beb30ee88ebc3f70e8e2612ed1f242af
DenPro117 <wangpanpan227@gmail.com>
2024-07-07 20:52:16 +0800

    Feat: lomo starter

---
+++ src/constants/HomeConstants.js
@@ -0,0 +1,7 @@
+export const WELCOME_HEADING = 'React Enterprise Starter Kit';
+export const IMG_ALT = 'React-Enterprise-Starter-Kit';
+export const REPO_URL = 'https://github.com/DenPro117/lomo-starter';
+export const GITHUB_FORK_TEXT = 'Fork on Github';
+export const LICENCE_BADGE = 'https://img.shields.io/github/license/DenPro117/react-enterprise-starter-kit?style=for-the-badge';
+export const STARS_BADGE = 'https://img.shields.io/github/stars/DenPro117/react-enterprise-starter-kit?style=for-the-badge';
+export const FORK_BADGE = 'https://img.shields.io/github/forks/DenPro117/react-enterprise-starter-kit?style=for-the-badge';
